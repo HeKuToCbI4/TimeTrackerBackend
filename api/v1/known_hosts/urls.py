fbc2b1c3363879865c95ff99174008d5489ea947
Nikita <leonov2424@mail.ru>
2023-10-23 23:44:17 +0200

    fixes for urls

--- api/v1/known_hosts/urls.py
+++ api/v1/known_hosts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("''/", views.KnownHostsListCreateAPI.as_view()),
+    path("", views.KnownHostsListCreateAPI.as_view()),
 ]
