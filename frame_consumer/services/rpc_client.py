6dc220da45f75908c1662ba47b53ededac0b5884
Nikita <leonov2424@mail.ru>
2023-10-26 02:36:16 +0200

    more small changes

--- frame_consumer/services/rpc_client.py
+++ frame_consumer/services/rpc_client.py
@@ -18,8 +18,8 @@ class RPCClientService:
         self.host = host
         self.port = port
         self.consumer_id = consumer_id
-        self.channel = None
-        self.service_stub = None
+        self.channel: grpc.Channel | None = None
+        self.service_stub: frame_info_service_grpc.FrameInfoServiceStub | None = None
         self.stop_requested = threading.Event()
 
     def connect(self):
@@ -27,9 +27,16 @@ class RPCClientService:
         if self.channel is None:
             self.channel = grpc.insecure_channel(f"{self.host}:{self.port}")
 
+    def create_stub(self):
+        if self.channel is None:
+            raise RPCClientServiceException(
+                "Client is not connected. Channel is not initialized."
+            )
+        self.service_stub = frame_info_service_grpc.FrameInfoServiceStub(self.channel)
+
     def _subscription_thread(self):
         subscribe_request = frame_info_service.StreamSubscribeRequest(self.consumer_id)
-        incoming_frame = frame_info.TimeFrameInfo
+        incoming_frame: frame_info.TimeFrameInfo
         # we need to get data, after that process and save it into db.
         while not self.stop_requested:
             try:
@@ -42,3 +49,24 @@ class RPCClientService:
                 retry_interval = 5
                 print(f"Retrying after {retry_interval}")
                 time.sleep(retry_interval)
+
+            self.create_stub()
+            # Receive and write data?
+            prev_frame: frame_info.TimeFrameInfo | None = None
+            for incoming_frame in self.service_stub.Subscribe(subscribe_request):
+                id = incoming_frame.id
+                process_path = incoming_frame.process_executable_path
+                timestamp = incoming_frame.utc_timestamp
+                process_window = incoming_frame.window_title
+                if prev_frame is None:
+                    print(f"Received first frame for host {self.host}")
+                    prev_frame = incoming_frame
+                    continue
+                # Process incoming frame considering previous received.
+                if id != prev_frame.id + 1:
+                    print(
+                        f"Sequnce error! Prev frame id = {prev_frame.id}. Current {id=}."
+                    )
+                    prev_frame = incoming_frame
+                    continue
+
