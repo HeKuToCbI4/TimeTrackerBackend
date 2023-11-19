6c95de12e34c5076d7ec23fbc886e75f6c27933b
Nikita <leonov2424@mail.ru>
2023-11-19 17:55:53 +0100

    some fixes for title processing

--- modules/title_preprocessor.py
+++ modules/title_preprocessor.py
@@ -16,19 +16,22 @@ class WindowTitlePreprocessor(object):
         if executable is None:
             return title
         if executable.lower() == "telegram.exe":
-            found = re.findall(
-                r"(\(\d+\))?\s?([\w\s,|\\/()\[\].!@#$%^&*]+)(.\s\(\d+\))?", title
-            )
-            if not found:
-                return title.strip()
-            if len(found) == 1:
-                return found[0][1]
-            else:
-                print("We're fucked!")
+            found_start = re.findall(r"^(\(\d+\))+", title)
+            print(f"{found_start=}")
+            if len(found_start) == 1:
+                title = title.replace(found_start[0], "")
+            found_end = re.findall(r"(\s.\s\(\d+\))+$", title)
+            print(f"{found_end=}")
+            if len(found_end) == 1:
+                title = title.replace(found_end[0], "")
+            found_end_2 = re.findall(r"(\(\d+\))+$", title)
+            print(f"{found_end_2=}")
+            if len(found_end_2) == 1:
+                title = title.replace(found_end_2[0], "")
+            return title.strip()
+
         if "youtube" in title.lower():
-            found = re.findall(
-                r"(\(\d+\))?\s+?(.+)", title
-            )
+            found = re.findall(r"(\(\d+\))?\s+?(.+)", title)
             if not found:
                 return title.strip()
             if len(found) == 1:
